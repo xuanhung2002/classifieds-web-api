fe18f6f18da532aeaa5b3a2324861f6d1d5ce948
Evan.Nguyen <evan.nguyen@avepoint.com>
2024-03-12 14:23:53 +0700

    implement repository base, unit of work pattern

---
+++ UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitOfWork
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        //IUserRepository Users { get; }
+        int Complete();
+    }
+}
